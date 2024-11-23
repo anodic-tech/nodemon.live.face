aeabfbdee7ec36319bba37cb72b43145295d0e31
justin.anodic <justin@anodic.tech>
2024-11-22 23:01:44 -0500

    add iframe injection

--- src/components/StreamEmbed.tsx
+++ src/components/StreamEmbed.tsx
@@ -1,7 +1,28 @@
+import { SyntheticEvent } from "react"
+import { createRoot } from "react-dom/client"
+
+const IframeStyles = () => {
+    const css = `
+    #player {
+        max-height: 'fit-content'
+    }
+    `
+    return <style>{css}</style>
+}
+
 export const StreamEmbed = () => {
+
+    const injectStyles = (e:SyntheticEvent<HTMLIFrameElement, Event>) =>{
+        const iframe = e.target as HTMLIFrameElement
+        if(!iframe?.contentDocument?.head) return
+        const root = createRoot(iframe.contentDocument.head)
+        root.render(<IframeStyles/>);
+    }
+
     return <iframe
         src="https://nodemon.live/embed/video"
         title="Owncast"
         referrerPolicy="origin"
-        allowFullScreen />
+        allowFullScreen 
+        onLoad={injectStyles}/>
 }
